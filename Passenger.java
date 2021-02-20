a337e3fc7db78b0600e228cc5c6868352c25d104
Saritakumari98 <75609120+Saritakumari98@users.noreply.github.com>
2021-02-21 01:17:35 +0530

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -2,17 +2,27 @@ package trainticket;
 
 import java.util.*;
 
+
+/*========  Implementation of Comparable Interface in Passenger class ===========*/
 public class Passenger implements Comparable<Passenger>
 {
+
+	/*========  Variable declaration ===========*/
 	private String name;
 	private int age;
 	private char gender;
+
+
+	/*=========  Constructor ================*/
 	public Passenger(String name, int age, char gender) {
 		super();
 		this.name = name;
 		this.age = age;
 		this.gender = gender;
 	}
+
+
+	/*=========   start of getter & setter methods======*/
 	public String getName() {
 		return name;
 	}
@@ -31,12 +41,17 @@ public class Passenger implements Comparable<Passenger>
 	public void setGender(char gender) {
 		this.gender = gender;
 	}
-	
+	/*==========  end of getter & setter methods =================*/
+
+
+	/*=============== Overriding of toString method ================= */
 	@Override
 	public String toString() {
-		return  name +"                  "+ age +"              "+ gender;
+		return  name +"                 "+age+"              "+gender;
 	}
-	
+
+
+	/*=============== Overriding of compareTo method of Comparable Interface================= */
 	@Override
 	public int compareTo(Passenger p) {
 		return name.compareTo(p.name);
