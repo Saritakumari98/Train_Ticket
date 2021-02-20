14525384156c23417ae91774fb7b595821e55fbb
Saritakumari98 <75609120+Saritakumari98@users.noreply.github.com>
2021-02-21 01:19:17 +0530

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -9,21 +9,22 @@ import java.time.format.DateTimeFormatter;
 
 public class Ticket {
 
-	private int counter=100;
+	/*========  Variable declaration ===========*/
+	private int counter;
 	private String pnr;
 	private LocalDate TravelDate;
 	private Train train;
-        private TreeMap <Passenger,Double>Passengers=new TreeMap();
+	private TreeMap <Passenger,Double>Passengers=new TreeMap();
 
 
-
+	/*=========  Constructor ================*/
 	public Ticket(LocalDate travelDate, Train train) {
 
 		TravelDate = travelDate;
 		this.train = train;
 	}
 
-
+	/*=========   start of getter & setter methods======*/
 	public int getCounter() {
 		return counter;
 	}
@@ -66,42 +67,33 @@ public class Ticket {
 		this.train = train;
 	}
 
+	/*==========  end of getter & setter methods =================*/
 
-	public  String generatePNR()
+
+	/*=============== generatePNR method generate the PNR  ================= */	
+	public  String generatePNR() 
 	{
-		
-		
+
+
 
 		String source=String.valueOf(train.getSource().charAt(0));
 		String destination=String.valueOf(train.getDestination().charAt(0));
 		String date=TravelDate.format(DateTimeFormatter.ofPattern("YYYYMMdd"));
-		 pnr= source+destination+"_"+date+"_"+counter;
-                 this.counter++;
-        // System.out.println(" pnr "+pnr);
+		pnr= source+destination+"_"+date+"_"+counter;
+
+
 		if(TravelDate.isAfter(LocalDate.now()))
 			return pnr;
 		else
-			return "Please Enter valid date";
-		/*
-		String date=TravelDate.format(DateTimeFormatter.ofPattern("YYYYMMdd"));
-		
-		StringBuilder sb=new StringBuilder();
-		System.out.println("train  "+train.getSource().charAt(0));
-		sb.append(train.getSource().charAt(0));
-		System.out.println("Source  "+sb);
-		sb.append(train.getDestination().charAt(0));
-		sb.append(date);
-		
-		System.out.println(sb);
-		
-		return sb.toString();*/
+			return "Travel Date is before current date ";
+
 
 	}
-
+	
+	
+	/*=============== calPassengerFare method calculate the one passenger price  ================= */
 	double calPassengerFare(Passenger Passenger)
 	{
-
-
 
 		if(Passenger.getAge()<=12)
 			return (0.5)*(train.getTicketprice());
@@ -117,71 +109,78 @@ public class Ticket {
 
 
 	}
-	//Passengers=new TreeMap();
+
+
+	/*=============== addPassenger method is used to add passenger for ticket booking ================= */
 	public void addPassenger(String name,int age,char gender)
 	{ 
-		
+
 		Double fare= calPassengerFare(new Passenger(name, age, gender));
-		//System.out.println(fare);
+
 		Passengers.put(new Passenger(name, age, gender),fare);
 	}
 
+
+	/*=============== calculateTotalTicketPrice method calculate the total price of passenger ================= */
 	double calculateTotalTicketPrice()
 	{
 		Double totalPrice=0.0;
-		//Collection<Double> price=Passengers.values();
-		
+
+
 		for(Double values:Passengers.values())
 		{
 			totalPrice=totalPrice+values;
-			//System.out.println(totalPrice);
+
 		}
-		
-		//System.out.println(totalPrice);
+
+
 		return totalPrice;
 
 	}
 
-	StringBuilder generateTicket()
+
+	/*=============== generateTicket method generate the ticket of passenger ================= */
+	StringBuilder generateTicket() 
 	{
 		Collection<Passenger> entrySet=Passengers.keySet();
+		String traveldate=TravelDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		StringBuilder sb=new StringBuilder();
-		
+
 		sb.append("PNR   :"+generatePNR()).append("\n")
 		.append("TrainNo :"+train.getTrainNo()).append("\n")
 		.append("Train Name :"+train.getTrainName()).append("\n")
 		.append("from :"+train.getSource()).append("\n")
 		.append("to :"+train.getDestination()).append("\n")
-		.append("travelDate :"+TravelDate).append("\n")
+		.append("travelDate :"+traveldate).append("\n")
 		.append("\n").append("\n").append("Passengers :")
 		.append("\n").append("Name    \t").append("     Age\t")
-		.append("    Gender").append("\tFare").append("\n");
-		 for(Passenger Key:entrySet)
-		 {
+		.append("    Gender").append(" Fare").append("\n");
+		for(Passenger Key:entrySet)
+		{
 			sb.append(Key+"    "+Passengers.get(Key));
 			sb.append("\n");
-		 }
-		 sb.append("\n")
+		}
+		sb.append("\n")
 		.append("totalPrice: "+calculateTotalTicketPrice());
 		return sb;
 
 	}
 
+	/*=============== writeTicket method  write the ticket of passenger in a file ================= */
 	public  void writeTicket() throws IOException
 	{
 		FileWriter f=new FileWriter(generatePNR()+".txt");
 		BufferedWriter bw=new BufferedWriter(f);
-		 bw.append(generateTicket());
-		 bw.flush();
-		 bw.close();
+		bw.append(generateTicket());
+		bw.flush();
+		bw.close();
 	}
 
-
+	/*=============== Overriding of toString method ================= */
 	@Override
 	public String toString() {
 		return "Ticket [counter=" + counter + ", pnr=" + pnr + ", TravelDate=" + TravelDate + ", train=" + train
 				+ ", Passengers=" + Passengers + "]";
 	}
-	
-	
+
 }
