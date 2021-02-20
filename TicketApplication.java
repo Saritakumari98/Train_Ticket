208caa3bd760d5c2b39aa7d34879b628d8aa615a
Saritakumari98 <75609120+Saritakumari98@users.noreply.github.com>
2021-02-20 13:00:10 +0530

    Update TicketApplication.java

--- TicketApplication.java
+++ TicketApplication.java
@@ -1,10 +1,10 @@
 package trainticket;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-import java.util.TreeMap;
-import java.util.Date;
+import java.util.*;
+
 
 public class TicketApplication {
 
@@ -16,53 +16,57 @@ public class TicketApplication {
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, NullPointerException{
 		Scanner sc=new Scanner(System.in);
+		
+		
 
-		System.out.println("======Welcome To Train Ticket Reservation=========");
+		System.out.println("======Welcome To Train Reservation=========");
 
-		System.out.println("Enter the Train Number");
-		int trainNo=sc.nextInt();
-		sc.nextLine();
-
-		System.out.println("Enter the Train Name");
-		String trainName=sc.nextLine();
-
-
+		
+        TrainDAO traindao=new TrainDAO();
+        System.out.println("Enter the train Number");
+		Train train =traindao.findTrain(sc.nextInt());
+		
+		
+		
 		System.out.println("Enter TravelDate");
 		String userInput=sc.next();
+		
 		LocalDate travelDate=dateInput(userInput);
-
-
+		Ticket ticket=new Ticket(travelDate,train);
+		
 		System.out.println("Enter Number of Passengers");
 		int numberOfPassengers=sc.nextInt();
 		
-
-		Train train =new Train(trainNo,trainName,"Banglore","Mumbai",2347.67);
-		Ticket ticket=new Ticket(travelDate,train);
-		
-                 sc.nextLine();
+        sc.nextLine();
 		for(int i=1;i<=numberOfPassengers;i++)
 		{
 			System.out.println("Enter Passenger Name");
 			String name=sc.next();
 			sc.nextLine();
-
+			
 			System.out.println("Enter Age");
 			int age=sc.nextInt();
-                        sc.nextLine();
-			
+             sc.nextLine();
+             
 			System.out.println("Enter Gender(M/F)");
 			String gender=sc.next();
 			char g=gender.charAt(0);
-
+           
+			
 			ticket.addPassenger(name, age, g);
+			
+			
 
 		}
+		
 
 		System.out.print("Ticket Booked with PNR:"+ticket.generatePNR());
-
-
+		ticket.writeTicket();
+		
+		//System.out.println("Ticket should be written to the file with filename :"+ ticket.generatePNR());     
+		
 
 
 
